863707ffa7dfec7dacca195a703401c5e07bc649
mpmeurville <marie.pierre.meurville@gmail.com>
2019-07-25 17:35:35 +0200

    Addition of the real time parameters selection

--- adjust_F_B_C.py
+++ adjust_F_B_C.py
@@ -5,69 +5,127 @@ import os
 
 
 
- #### AIM OF THIS SCRIPT ####
+#### AIM OF THIS SCRIPT ####
 # I want to be able to take pictures from a camera, and to adjust contrast, brightness and focus easily, before taking real images. 
 # The idea is to iteratively ask the user to set a value for these 3 parameters, 
 # and once the user is happy with it, to remember the values, and pass them as an argument for the actual recording. 
 
 
-
-cap  = cv2.VideoCapture(0)
-
-
-main = raw_input ('Do you want to set parameters? (Y, N, A)') # When python3, change to input
-
-if main == 'A':
-	cap.set(3, width)
-	cap.set(4, height)
-	ret, frame = cap.read()
-	cv2.imshow('frame', frame)
-	print ('F: ', cap.get(cv2.CAP_PROP_FOCUS), 'B: ',cap.get(cv2.CAP_PROP_BRIGHTNESS), 'C: ', cap.get(cv2.CAP_PROP_CONTRAST))
-
-	if cv2.waitKey(20) & 0xFF == ord('q'):
-		print("Q")
-		#break
+# Default parameters
+f = 0.32
+b = 0.7
+c = 0.5
 
 
+main = input ('Do you want to set parameters? (Yes, Default, Auto) :	') # When python3, change to input
 
 
-# Enable (1) or disable (0) autofocus. 
-#cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
-#cap.set(cv2.CAP_PROP_AUTOEXPOSURE, 0)
+if main == 'Auto':
+
+	val = False
+
+	cap  = cv2.VideoCapture(0)
+
+	cap.set(3, 1920)
+	cap.set(4, 1080)
 
 
-#cap.set(3, width)
-#cap.set(4, height)
+	if not cap.isOpened():
+        	raise Exception("Could not open video device")
 
-#foc_ok = False
-#bri_ok = False
-#con_ok = False
+	while True:
+		ret, frame = cap.read()
+		cv2.imshow('Auto Settings', frame)
 
-#while (ret):
-#	if foc_ok == False:
-#		foc = input('What value for FOCUS?')
-#
-#	if bri_ok == False:
-#		bri = input('What value for BRIGHTNESS?')
-#
-#	if con_ok = False:
-#		con = input('What value for CONTRAST?')
-#
-#	cap.set(cv2.CAP_PROP_FOCUS, foc)
-#	cap.set(cv2.CAP_PROP_BRIGHTNESS, bri)
-#	cap.set(cv2.CAP_PROP_CONTRAST, con)
-#
-#	ret, frame = cap.read()
-#
-#	F = input('Is FOCUS ok?')
-#	B = input ('Is BRIGHTNESS ok?')
-#	C = input ('Is CONTRAST ok?')
-#
-#	# Keyboard interrupt in case of emergency !!!
-#	if cv2.waitKey(20) & 0xFF == ord('q'):
-#                       break
+		if val == False:
+			print ('F: ', cap.get(cv2.CAP_PROP_FOCUS),'	', 'B: ',cap.get(cv2.CAP_PROP_BRIGHTNESS), '	', 'C: ', cap.get(cv2.CAP_PROP_CONTRAST))
+			val = True
+		if cv2.waitKey(20) & 0xFF == ord('q'):
 
-#cap.release()
-#cv2.destroyAllWindows()
+			break
+	cv2.destroyAllWindows()
+
+if main == 'Default':
+
+	val = False
+
+	cap  = cv2.VideoCapture(0)
+
+	cap.set(3, 1920)
+	cap.set(4, 1080)
+
+	cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+
+	if not cap.isOpened():
+		raise Exception("Could not open video device")
+
+	while True:
+
+		ret, frame = cap.read()
+
+		cap.set(cv2.CAP_PROP_FOCUS,f)
+		cap.set(cv2.CAP_PROP_BRIGHTNESS,b)
+		cap.set(cv2.CAP_PROP_CONTRAST,c)
+
+		cv2.imshow('Default Settings', frame)
+
+		if val == False:
+			print ('F: ', cap.get(cv2.CAP_PROP_FOCUS),'     ', 'B: ',cap.get(cv2.CAP_PROP_BRIGHTNESS), '    ', 'C: ', cap.get(cv2.CAP_PROP_CONTRAST))
+			val = True
+
+		if cv2.waitKey(20) & 0xFF == ord('q'):
+
+			break
+	cv2.destroyAllWindows()
 
 
+if main == 'Yes':
+
+	val = False
+
+	f = input ('F :    ')
+	b = input ('B :    ')
+	c = input ('C :    ')
+
+
+	cap  = cv2.VideoCapture(0)
+
+	cap.set(3, 640)
+	cap.set(4, 480)
+
+	cap.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+
+	if not cap.isOpened():
+		raise Exception("Could not open video device")
+
+	while True:
+
+		ret, frame = cap.read()
+
+		cap.set(cv2.CAP_PROP_FOCUS, float(f))
+		cap.set(cv2.CAP_PROP_BRIGHTNESS, float(b))
+		cap.set(cv2.CAP_PROP_CONTRAST, float(c))
+
+		cv2.imshow('User Settings', frame)
+
+		if val == False:
+			print ('F: ', cap.get(cv2.CAP_PROP_FOCUS),'     ', 'B: ',cap.get(cv2.CAP_PROP_BRIGHTNESS), '    ', 'C: ', cap.get(cv2.CAP_PROP_CONTRAST))
+			val = True
+
+		if cv2.waitKey(20) & 0xFF == ord('q'):
+
+			break
+
+		if cv2.waitKey(20) & 0xFF == ord('c'):
+
+			cv2.destroyAllWindows()
+
+			if not cap.isOpened():
+				raise Exception("Could not open video device")
+
+			f = input ('F :    ')
+			b = input ('B :    ')
+			c = input ('C :    ')
+
+
+	cv2.destroyAllWindows()
